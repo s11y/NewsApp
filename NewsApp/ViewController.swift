986c4a044740506ee364e144a414488b79609e4a
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-01-15 14:27:53 +0900

    initial commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by ShinokiRyosei on 2017/01/15.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - ViewController
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+}
+
